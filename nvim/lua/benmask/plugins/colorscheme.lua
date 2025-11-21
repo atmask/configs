d7fd7e6291fe1dd23496ac583f4c1f2139af1b64
atmask <benjaminajm@gmail.com>
2025-11-21 09:27:08 -0500

    Refactor the scheme picking

--- nvim/lua/benmask/plugins/colorscheme.lua
+++ nvim/lua/benmask/plugins/colorscheme.lua
@@ -1,126 +1,148 @@
--- To switch colorschemes, comment out one return block and uncomment the other
+-- ============================================================================
+-- COLORSCHEME SELECTOR
+-- Change this variable to switch colorschemes: "kanagawa", "catppuccin", "poimandres", "tokyonight"
+-- ============================================================================
+local active_colorscheme = "kanagawa"
 
--- OPTION 1: Kanagawa Dragon (Currently Active - Darkest)
-return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-    require("kanagawa").setup({
-      compile = false,
-      undercurl = true,
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,
-      dimInactive = false,
-      terminalColors = true,
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
+-- ============================================================================
+-- COLORSCHEME CONFIGURATIONS
+-- ============================================================================
+
+local colorschemes = {
+  kanagawa = {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,
+        dimInactive = false,
+        terminalColors = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
             },
           },
         },
-      },
-      theme = "dragon", -- dragon is the darkest variant (also: wave, lotus)
-      background = {
-        dark = "dragon",
-      },
-    })
+        theme = "dragon", -- dragon is the darkest variant (also: wave, lotus)
+        background = {
+          dark = "dragon",
+        },
+      })
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end,
+  },
 
-    vim.cmd.colorscheme("kanagawa-dragon")
-  end,
+  catppuccin = {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha (mocha is darkest)
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = true,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+        },
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          telescope = {
+            enabled = true,
+          },
+          mason = true,
+          which_key = true,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = false,
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  poimandres = {
+    "olivercederborg/poimandres.nvim",
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        bold_vert_split = false, -- use bold vertical separators
+        dim_nc_background = false, -- dim 'non-current' window backgrounds
+        disable_background = false, -- disable background
+        disable_float_background = false, -- disable background for floats
+        disable_italics = false, -- disable italics
+      })
+      vim.cmd("colorscheme poimandres")
+    end,
+  },
+
+  tokyonight = {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      local transparent = false
+
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+
+      require("tokyonight").setup({
+        style = "night",
+        transparent = transparent,
+        styles = {
+          sidebars = transparent and "transparent" or "dark",
+          floats = transparent and "transparent" or "dark",
+        },
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = transparent and colors.none or bg_dark
+          colors.bg_float = transparent and colors.none or bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = transparent and colors.none or bg_dark
+          colors.bg_statusline = transparent and colors.none or bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
 }
 
--- OPTION 2: Catppuccin Mocha (Dark)
--- Uncomment this block and comment out the above to use Catppuccin
---[[ return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha (mocha is darkest)
-      transparent_background = false,
-      show_end_of_buffer = false,
-      term_colors = true,
-      dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-      },
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        telescope = {
-          enabled = true,
-        },
-        mason = true,
-        which_key = true,
-        indent_blankline = {
-          enabled = true,
-          colored_indent_levels = false,
-        },
-      },
-    })
-
-    vim.cmd.colorscheme("catppuccin")
-  end,
-} ]]
-
--- OPTION 3: Tokyonight (Custom Blue)
--- Uncomment this block and comment out the above to use Tokyonight
---[[ return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = false
-
-    local bg = "#011628"
-    local bg_dark = "#011423"
-    local bg_highlight = "#143652"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
-
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
-      end,
-    })
-
-    vim.cmd("colorscheme tokyonight")
-  end,
-} ]]
+-- ============================================================================
+-- RETURN THE SELECTED COLORSCHEME
+-- ============================================================================
+return colorschemes[active_colorscheme]
